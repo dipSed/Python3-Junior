70377e41d823ffbc0e898687124b8409fbb2d28a
dipSed <43323858+dipSed@users.noreply.github.com>
2018-10-29 17:57:25 +0300

    Create z_1.py

---
+++ lesson 7/z_1.py
@@ -0,0 +1,9 @@
+from random import randint
+for n in range(1,13):
+    for m in range (4):
+        q=randint(2,5)
+        w=randint(2,5)
+        e=randint(2,5)
+        r=q+w+e
+    print (n,"|",q,w,e,"cymm = ",r,end=" ")
+    print()
