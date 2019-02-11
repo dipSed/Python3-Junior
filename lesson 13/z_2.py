d3083640146b78adf675cc1573ef30c8d2f4d3c2
dipSed <43323858+dipSed@users.noreply.github.com>
2019-02-11 18:21:25 +0300

    Create z_2.py

---
+++ lesson 13/z_2.py
@@ -0,0 +1,13 @@
+import turtle
+def V ():
+    for i in range (2):
+        turtle.forward(20)
+        turtle.right(90)
+        turtle.forward(60)
+        turtle.right(90)
+
+turtle.reset()
+turtle.up()
+turtle.goto(-50,0)
+turtle.down()
+V()
